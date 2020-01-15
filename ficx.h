c51b492286914e318369b4ebbfda34ce8f900da9
Dallin Dahl <dallinjdahl@gmail.com>
2020-01-15 16:28:00 -0700

    minimum viable product

--- ficx.h
+++ ficx.h
@@ -1,71 +1,93 @@
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdio.h>
 //fixed point arithmetic library
 typedef int16_t fixed; //choose your width, and specify the double width type
 typedef int32_t dfixed; 
-#define bpoint 6 //binary point places
+#define bpoint 9 //binary point places
 #define SWAP(a, b) (((a) ^= (b)), ((b) ^= (a)), ((a) ^= (b)))
 
 
 //multiply and divide
-#define mult(x, y) ((fixed)((dfixed)x*(dfixed)y>>bpoint)) //a/x*b/x in terms of x: (ab/x)/x
-#define quot(x, y) ((fixed)(((dfixed)x<<bpoint)/(dfixed)y)) //a/x / b/x in terms of x: (ax/b)/x
+#define fmul(x, y) ((fixed)((dfixed)x*(dfixed)y>>bpoint)) //a/x*b/x in terms of x: (ab/x)/x
+#define fdiv(x, y) ((fixed)(((dfixed)x<<bpoint)/(dfixed)y)) //a/x / b/x in terms of x: (ax/b)/x
 
 //convert number to fixed point
 #define fix(x) ((fixed)x<<bpoint)
 
 //output routines
-//output first the whole number, then the point, and then the fractional point
 #define whole(x) (x>>bpoint)
 #define pointmask ((1<<bpoint)-1)
 #define frac(x) (x&pointmask)
 
+char buf[15];
+
 fixed fxsqrt(fixed n) {
+	if(n < 0) {
+		return fix(-1);
+	}
+	if(n == 0) {
+		return 0;
+	}
 	fixed x = n >> 1;
 	fixed y = 0;
 	for(;;) {
-		y = (x + n/x) >> 1;
+		y = (x + fdiv(n,x)) >> 1;
 		if( y >= x) {
-			return x << (bpoint >> 1);
+			return x;// << (bpoint >> 1);
 		}
 		x = y;
 	}
 }
 
+//output first the whole number, then the point, and then the fractional point
 char *fxtoa(fixed f, char *out){
-	fixed i = whole(f);
 	div_t d;
+	char *res = out;
+	char *head = out-1;
 	uint8_t j = 0;
-	char *head = out;
 	if(f < 0 && !*out) {
 		*(out++) = '-';
 		f = ~f + 1;
+		head++;
 	}
-	while(d.quot != 0 && !*out) {
+
+	fixed i = whole(f);
+
+	if(*out == -1) {
+		return res;
+	}
+	do {
 		d = div(i, 10);
 		*(out++) = d.rem + '0';
-		j++;
 		i = d.quot;
-	}
+		j++;
+	} while(d.quot != 0 && ~*out);
+
 	char *foot = out;
 	j /= 2;
-	for(; j; j--) {
-		SWAP(*(head++), *(foot--)); 
-	};
-	
-	if(*out) {
-		return out;
+
+	while (j) {
+		char c = head[j];
+		head[j] = foot[-j];
+		foot[-j] = c;
+		j--;
 	}
-	
+
 	*(out++) = '.';
-	fixed fr = frac(f);
-	uint8_t dig; 
-	for(; fr > 0 && !*out; fr -= fix(dig)) {
-		fr *= 10;
-		dig = whole(fr);
-		*(out++) = dig + '0';
-	}
 	
+	if(*out == -1) {
+		return res;
+	}
+
+	f = frac(f) * 10;
+	do {
+		i = whole(f);
+		*(out++) = i + '0';
+		f = frac(f) * 10;
+	} while(f != 0 && ~*out);
+
 	*out = '\0';
-	return out;
+	return res;
 }
+
--- test.c
+++ test.c
@@ -1,9 +1,10 @@
 #include "ficx.h"
 
 int main(int argc, char **argv) {
-	char strbuf[15];
+	char strbuf[15] = {0};
 	strbuf[14] = -1;
-	fixed x = fix(169);
+	fixed x = fix(1);
+	x >>= 1;
 	fixed y = fxsqrt(x);
 	printf("sqrt(%s)", fxtoa(x,strbuf));
 	printf(": %s\n", fxtoa(y,strbuf));
